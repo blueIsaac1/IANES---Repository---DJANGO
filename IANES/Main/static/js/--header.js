e56fbfa281341524e799d62356090853860fca5e
blueIsaac1 <isaaccleitondasilva@gmail.com>
2024-11-07 10:34:40 -0300

    commit do chico

--- IANES/Main/static/js/--header.js
+++ IANES/Main/static/js/--header.js
@@ -1,3 +1,25 @@
+// Função para calcular e aplicar o tamanho do header
+function ajustarHeader() {
+    const root = document.documentElement;
+    const headerBot_size = parseFloat(getComputedStyle(root).getPropertyValue('--tamanho_header_bot'));
+    const headerTop_size = parseFloat(getComputedStyle(root).getPropertyValue('--tamanho_header_top'));
+    let tamanhoHeader;
+    
+    // Verificar se é a página index
+    const headerBot = document.getElementById("header_bot")
+    const headerBot_size_pageValue = headerBot ? headerBot.getAttribute("aria-page-value") : null;
+
+    if (headerBot_size_pageValue === "index") {
+        tamanhoHeader = headerBot_size + headerTop_size;
+    } else {
+        tamanhoHeader = headerTop_size;
+    }
+
+    // Atualizar a variável CSS do header
+    root.style.setProperty('--tamanho_header', `${tamanhoHeader}vw`);
+    return `${tamanhoHeader}vw`;
+}
+
 // Função para criar cabeçalho assistente
 window.header_assistente = function(tamanho_topo) { 
     let prevScrollPos = window.pageYOffset;
@@ -11,4 +33,7 @@ window.header_assistente = function(tamanho_topo) {
         prevScrollPos = currentScrollPos;
     };
 };
-window.addEventListener("scroll", header_assistente("6vw"))
+
+// Ajustar header e iniciar função de cabeçalho assistente
+let tamanho_topo = ajustarHeader();
+window.addEventListener("scroll", header_assistente(tamanho_topo))
