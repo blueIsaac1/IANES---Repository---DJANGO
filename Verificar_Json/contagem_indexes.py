6927288e24703a99bfc390384fafe482d54e1b9c
Pedro-B-Siqueira <pedrosiqueirarp@gmail.com>
2024-11-12 16:43:07 -0300

    12/11 - Commit Pedro - Comentários

--- Verificar_Json/contagem_indexes.py
+++ Verificar_Json/contagem_indexes.py
@@ -1,19 +1,16 @@
 import json
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
+
     WARNING = '\033[93m'
-    FAIL = '\033[91m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+
+jsons = ['conteudo_anp', 'conteudo_bndes', 'conteudo_embrapii', 'conteudo_fapesp', 'conteudo_FCO',
+         'conteudo_finep', 'conteudo_funcafe', 'conteudo_leidobem', 'conteudo_pronamp', 'conteudo_pronampe']
 
 def contagem_indexes(colecao):
     # Carregar o conteúdo do arquivo JSON com a codificação correta
-    with open(f'json/{colecao}.json', 'r', encoding='utf-8') as file:
+    with open(f'DADOS/{colecao}.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
 
     # Contar o número de índices (chaves) no dicionário
@@ -21,12 +18,9 @@ def contagem_indexes(colecao):
 
     print(f'Número de índices presentes no JSON {bcolors.WARNING}{colecao}{bcolors.ENDC}: {num_indices}')
 
-contagem_indexes('conteudo_anp')
-contagem_indexes('conteudo_bnds')
-contagem_indexes('conteudo_embrapii')
-contagem_indexes('conteudo_fapesp')
-contagem_indexes('conteudo_finep')
-contagem_indexes('conteudo_leidobem')
+
+for nome in jsons:
+    contagem_indexes(f'{nome}')
 
 
 
